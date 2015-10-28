c8993ba43ec6cc577bda4fd7e1636335e90e0f39
dinesh <dineshpy07@gmail.com>
2015-10-28 13:59:13 +0530

    Check from api if PR is mergeable

--- green_bot.py
+++ green_bot.py
@@ -20,11 +20,10 @@ def add_log(msg):
 
 # Check if bot has already added a comment
 def has_bot_comment(pull_number):
-    resp = requests.post('https://api.github.com/repos/%s/%s/issues/%d/comments' %(owner, repo, pull_number))
+    resp = requests.get('https://api.github.com/repos/%s/%s/issues/%d/comments' %(owner, repo, pull_number))
+    json_resp = json.loads(resp.text)
 
-    json_resp = json.loads(resp)
-
-    for comment in range(len(json_resp)):
+    for comment in json_resp:
         if comment.get('user').get('login') == DEVLOG_BOT:
             return True
     return False
@@ -43,10 +42,6 @@ def create_comment(pull_number, message, access_token):
                   headers=headers)
 
 def merge_pr(pull_number, sha, title):
-    # Parsing config
-    cfg = json.load(open('green.cfg'))
-    access_token = cfg.get('access_token')
-
     # Process title to remove any use of breaks build
     title = title.replace('breaks build 🚨', 'update')
 
@@ -65,22 +60,37 @@ def merge_pr(pull_number, sha, title):
     resp_text = json.loads(resp.text)
     add_log(resp_text)
 
-    if resp_text.get('message') == 'Pull Request is not mergeable' and not has_bot_comment:
-        create_comment(pull_number, "This PR cannot be merged by me(bot).", access_token)
-        
     # Delay of 3 mins (180 secs)
     time.sleep(180)
 
 def check_and_merge(pull_number, ref, sha, title):
+    # Check if CI tests have passed
     status = requests.get('https://api.github.com/repos/%s/%s/commits/%s/status' %(owner, repo, ref))
-    
+
     stat_content = json.loads(status.text)
     status = stat_content.get('state')
     
-    if status == 'success':
+    # Check if the current PR is mergeable
+    single_pr = requests.get('https://api.github.com/repos/%s/%s/pulls/%d' %(owner, repo, pull_number))
+
+    single_pr_content = json.loads(single_pr.text)
+    is_mergeable = single_pr_content.get('mergeable')
+
+    if status == 'success' and not is_mergeable and not has_bot_comment(pull_number):
+        create_comment(pull_number, "This PR cannot be merged by me(bot).", access_token)
+        return
+    elif status == 'success' and is_mergeable:
         merge_pr(pull_number, sha, title)
 
 def main():
+    # Parsing config
+    cfg = json.load(open('green.cfg'))
+    access_token = cfg.get('access_token')
+
+    if access_token == None:
+        add_log('Failed to fetch access token')
+        return
+
     pull_requests = requests.get('https://api.github.com/repos/%s/%s/pulls' %(owner, repo))
     
     pr_content = json.loads(pull_requests.text)
@@ -100,4 +110,3 @@ def main():
             
 if __name__ == "__main__":
     main()
-
